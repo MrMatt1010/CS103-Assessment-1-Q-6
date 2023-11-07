4c9b93cd7ecf16ccecbb276d8196d6a9dc479109
Matt T <mrsymtec@gmail.com>
2023-11-07 16:57:03 +1300

    Added Struct and function to calculate the expenses

--- CS103 Assessment 1 Q 6/CS103 Assessment 1 Q 6.cpp
+++ CS103 Assessment 1 Q 6/CS103 Assessment 1 Q 6.cpp
@@ -3,6 +3,24 @@
 // This program allows users to track their finances daily and weekly expenses as per the brief
 
 #include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+// Structure to store daily expenses
+struct dailyExpenses {
+    string date;
+    double transportCost;
+    double mealCost;
+    double entertainmentCost;
+    double otherCost;
+};
+
+// Function to claculate daily expenses
+double calaulateDailyExpenses(const dailyExpenses& expenses) {
+    return expenses.transportCost + expenses.mealCost + expenses.entertainmentCost + expenses.otherCost;
+}
+
 
 int main()
 {
